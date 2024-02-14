1c310c02bba882774d1c4ab1a7879f31be464a57
Geethanjali Gandu <geethanjali.gandu@publicissapient.com>
2024-02-14 15:08:53 +0530

    modified java file

--- Add.java
+++ Add.java
@@ -1,5 +1,8 @@
 public class Add{
     public static void main(String[] args){
         System.out.println("Hello");
+        int a=10;
+        int b=20;
+        System.out.println("addition"+(a+b));
     }
 }
